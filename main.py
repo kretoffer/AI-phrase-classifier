0beaed190cc830cd7f6938fc1c92f6327404b245
Kretov Artem <20kretovartem000@gmail.com>
2025-07-02 11:39:30 +0300

    add tokinizator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
 from src.template_to_hand import template2hand
+from src.tokinizator import *
 
 def main(path: str):
     with open(path, 'r', encoding='utf-8') as file:
@@ -11,7 +12,13 @@ def main(path: str):
     del(data["template-data"])
 
     phrases = [el["text"] for el in data["hand-data"]]
-    print(phrases)
+    phrases = [re.sub(r'[^\w\s]', '', phrase.lower(), flags=re.UNICODE) for phrase in phrases]
+
+    vocab = get_vocab(phrases)
+    idx = get_word2idx(vocab)
+
+    tokenize_phrases = [tokenize(phrase, idx) for phrase in phrases]
+    print(tokenize_phrases)
 
 
 
---
+++ src/tokinizator.py
@@ -0,0 +1,28 @@
+from typing import List
+import re
+
+def get_vocab(phrases: List[str]):
+    vocab = set()
+    for el in phrases:
+        text = re.sub(r'[^\w\s]', '', el.lower(), flags=re.UNICODE)
+        words = text.split(" ")
+        vocab.update(words)
+    return vocab
+
+
+def get_word2idx(vocab):
+    return {word: idx+1 for idx, word in enumerate(vocab)}
+
+def tokenize(text, vocab, max_len=32):
+    tokens = text.lower().split()
+    token_ids = [vocab.get(word, 0) for word in tokens] #0 — для неизвестных слов
+    token_ids = token_ids[:max_len] + [0] * max(0, max_len - len(token_ids)) #паддинг и обрезка
+    return token_ids
+
+if __name__ == "__main__":
+    custom_vocab = ["привет", "как", "дела", "погода", "сегодня", "отличная"]
+    word2idx = get_word2idx(custom_vocab)
+
+    text = "привет как погода"
+    tokens = tokenize(text, word2idx)
+    print(tokens)
