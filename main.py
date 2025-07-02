b98f40add682b44fa12cf1f61b8878399aa853f2
Kretov Artem <20kretovartem000@gmail.com>
2025-07-02 22:17:43 +0300

    add minimal api

--- main.py
+++ main.py
@@ -1,26 +1,138 @@
+from fastapi import FastAPI, Body, Header, Query
+
+from typing import Optional, Iterable, List
+import os
+import yaml
+from secrets import token_urlsafe
 import json
 
-from src.template_to_hand import template2hand
-from src.tokinizator import *
+from config import projects_dir
 
-def main(path: str):
-    with open(path, 'r', encoding='utf-8') as file:
-        data: dict = json.load(file)
+
+app = FastAPI()
+
+if not os.path.exists(projects_dir):
+    os.makedirs(projects_dir)
+
+
+def validate_request_post(userID, data: dict, elements_in_data: Iterable):
+    if not userID:
+        return {"error": "user identifier not specified"}
+    if not all([el in data for el in elements_in_data]):
+        return {"error": "not all data specified"}
+    if not os.path.exists(projects_dir + "/" + str(userID)):
+        return {"error": "user not registered"}
+
+
+def validate_request_get(userID):
+    if not userID:
+        return {"error": "user identifier not specified"}
+    if not os.path.exists(projects_dir + "/" + str(userID)):
+        return {"error": "user not registered"}
+
+
+@app.post("/new_project")
+def new_project(data: dict = Body(), userID: str = Header("admin", alias="userID")):
+    if resp := validate_request_post(userID, data, ("name",)):
+        return resp
+
+    if os.path.exists(projects_dir+"/"+str(userID)+"/"+str(data["name"])):
+        return {"error": "a project with this name already exists"}
     
-    for template in data["template-data"]:
-        data["hand-data"].extend(template2hand(template))
-    del(data["template-data"])
+    os.makedirs(projects_dir+"/"+str(userID)+"/"+str(data["name"]))
+    os.makedirs(f"{projects_dir}/{userID}/{data['name']}/models")
 
-    phrases = [el["text"] for el in data["hand-data"]]
-    phrases = [re.sub(r'[^\w\s]', '', phrase.lower(), flags=re.UNICODE) for phrase in phrases]
+    with open(f"{projects_dir}/{userID}/user.yaml", "r+", encoding="utf-8") as f:
+        user_data = yaml.safe_load(f)
+        user_data["projects"].append(str(data["name"]))
+        f.seek(0)
+        f.truncate()
+        yaml.dump(user_data, f, allow_unicode=True, sort_keys=False)
+    
+    with open(f"{projects_dir}/{userID}/{data["name"]}/config.yaml", "w", encoding="utf-8") as f:
+        project_config = {
+            "hidden_layer": 50,
+            "epohs": 5,
+            "learning_rate": 0.01,
+            "intents": [],
+            "entities": [],
+            "token": token_urlsafe(32)
+        }
+        yaml.dump(project_config, f, allow_unicode=True, sort_keys=False)
 
-    vocab = get_vocab(phrases)
-    idx = get_word2idx(vocab)
+    with open(f"{projects_dir}/{userID}/{data["name"]}/dataset.json", "x", encoding="utf-8") as f:
+        f.write('{"hand-data": [], "template-data": []}')
 
-    tokenize_phrases = [tokenize(phrase, idx) for phrase in phrases]
-    print(tokenize_phrases)
+    
+    return {"success": "good"}
+
+@app.post("/update_dataset")
+def update_dataset(data: dict = Body(), userID: str = Header("admin", alias="userID")):
+    if resp := validate_request_post(userID, data, ("project", "hand-data", "template-data")):
+        return resp
+    
+    if not os.path.exists(projects_dir+"/"+str(userID)+"/"+str(data["project"])):
+        return {"error": "no such project exists"}
+    
+    with open(f"{projects_dir}/{userID}/{data['project']}/dataset.json", "r+", encoding="utf-8") as f:
+        dataset = json.load(f)
+        dataset["hand-data"].extend(data["hand-data"])
+        dataset["template-data"].extend(data["template-data"])
+        f.seek(0)
+        f.truncate()
+        json.dump(dataset, f, ensure_ascii=False, indent=1)
+
+    return {"success": "good"}
+
+@app.get("/start_education")
+def start_education(userID: str = Header("admin", alias="userID")):
+    ...
 
 
+@app.post("/create_intent")
+def create_intent(data: dict = Body(), userID: str = Header("admin", alias="userID")):
+    if resp := validate_request_post(userID, data, ("project", "intents",)):
+        return resp
+    
+    if not os.path.exists(projects_dir+"/"+userID+"/"+str(data["project"])):
+        return {"error": "no such project exists"}
+    
+    with open(f"{projects_dir}/{userID}/{data["project"]}/config.yaml", "r+", encoding="utf-8") as f:
+        project_data = yaml.safe_load(f)
+        project_data["intents"].extend(data["intents"])
+        f.seek(0)
+        f.truncate()
+        project_data["intents"] = list(set(project_data["intents"]))
+        yaml.dump(project_data, f, allow_unicode=True, sort_keys=False)
 
-if __name__ == "__main__":
-    main("input.json")
+    return {"success": "good"}
+    
+
+@app.post("/create_entity")
+def create_entity(data: dict = Body(), userID: str = Header("admin", alias="userID")):
+    if resp := validate_request_post(userID, data, ("project", "entities",)):
+        return resp
+    
+    if not os.path.exists(projects_dir+"/"+str(userID)+"/"+str(data["project"])):
+        return {"error": "no such project exists"}
+    
+    with open(f"{projects_dir}/{userID}/{data["project"]}/config.yaml", "r+", encoding="utf-8") as f:
+        project_data = yaml.safe_load(f)
+        project_data["entities"].extend(data["entities"])
+        f.seek(0)
+        f.truncate()
+        project_data["entities"] = list(set(project_data["entities"]))
+        yaml.dump(project_data, f, allow_unicode=True, sort_keys=False)
+
+    return {"success": "good"}
+
+
+@app.get("/about_me")
+def about_me(userID: str = Header("admin", alias="userID")):
+    if resp := validate_request_get(userID):
+        return resp
+
+    with open(f"{projects_dir}/{userID}/user.yaml") as f:
+        user_data = yaml.load(f, Loader=yaml.SafeLoader)
+    
+    return user_data
